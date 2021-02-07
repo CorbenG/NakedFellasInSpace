cd11d1f668db3f8f8132022e2d3efa2178f0f2b3
ash_kmn <kamensky@sheridancollege.ca>
2021-02-07 12:36:59 -0500

    Add projectiles and shooting
    add projectiles containing ammo information
    add shooting with selected ammo type
    add projectile despawning

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -8,6 +8,8 @@ public class PlayerShooting : MonoBehaviour
     int AmmoType = 0;
 
     public GameObject AmmoTypeDisplay;
+    public GameObject Projectile;
+    public GameObject Camera;
     public float DisplayLerpSpeed;
     
     // Start is called before the first frame update
@@ -19,20 +21,45 @@ public class PlayerShooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        HandleAmmoTypeDisplay();
+        HandleShooting();
+        
+    }
 
+    void HandleAmmoTypeDisplay()
+    {
         if (Input.GetAxis("Mouse ScrollWheel") != 0)
         {
             if (Input.GetAxis("Mouse ScrollWheel") > 0)
             {
-                AmmoType = (AmmoType + 1)%4;
+                AmmoType += AmmoType;
             }
             if (Input.GetAxis("Mouse ScrollWheel") < 0)
             {
-                AmmoType = (AmmoType - 1)%4;
+                AmmoType -= AmmoType;
             }
+
+            if (AmmoType >= 4) AmmoType -= 4;
+            if (AmmoType <= -1) AmmoType += 4;
         }
 
-        AmmoTypeDisplay.transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(AmmoTypeDisplay.transform.localEulerAngles.z, 90.0f * AmmoType, Time.deltaTime * DisplayLerpSpeed) );
-        
+        AmmoTypeDisplay.transform.localEulerAngles = new Vector3(0, 0, Mathf.LerpAngle(AmmoTypeDisplay.transform.localEulerAngles.z, 90.0f * AmmoType, Time.deltaTime * DisplayLerpSpeed));
+    }
+
+    void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 MousePosition = Input.mousePosition;
+            MousePosition.z = 0.0f;
+            Vector3 WorldMousePosition = Camera.GetComponent<Camera>().ScreenToWorldPoint(MousePosition);
+            Vector3 ProjectileDirection = new Vector3(WorldMousePosition.x - transform.position.x, WorldMousePosition.y - transform.position.y, 0).normalized;
+
+
+            GameObject NewProjectile = Instantiate(Projectile, transform.position, Quaternion.identity);
+            NewProjectile.GetComponent<ProjectileMover>().MovementDirection = ProjectileDirection;
+            NewProjectile.GetComponent<ProjectileMover>().AmmoType = AmmoType;
+            // Debug.Log(AmmoType);
+        }
     }
 }
---
+++ Assets/ProjectileMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 MovementDirection;
+    public float MovementSpeed;
+    public float RotationSpeed;
+    public GameObject[] sprites;
+    public int AmmoType;
+    Rigidbody2D body;
+    IEnumerator Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprites[0].SetActive(AmmoType == 0);
+        sprites[1].SetActive(AmmoType == 1);
+        sprites[2].SetActive(AmmoType == 2);
+        sprites[3].SetActive(AmmoType == 3);
+        yield return StartCoroutine("WaitAndDestroy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.MovePosition(transform.position + MovementDirection * Time.deltaTime * MovementSpeed);
+        body.MoveRotation(body.rotation + Time.deltaTime * RotationSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "WorldBorder")
+        {
+            //Debug.Log("destroyed self due to reaching world border");
+            //Destroy(gameObject);
+        }
+    }
+
+    IEnumerator WaitAndDestroy()
+    {
+        yield return new WaitForSeconds(5);
+        Debug.Log("destroyed self due to timeout");
+        Destroy(gameObject);
+    }
+}
