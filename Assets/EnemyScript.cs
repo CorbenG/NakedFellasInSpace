8fe93577218078f6c4a3b3b90e8337567c271c75
CorbenG <61744834+CorbenG@users.noreply.github.com>
2021-02-08 18:37:14 -0500

    Add Score system

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public GameController Game;
+    ProjectileMover projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if(hit.gameObject.GetComponent<ProjectileMover>() != null)
+        {
+            Game.score += 100;
+            Destroy(hit.gameObject);
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public int score;
+    public Text scoretext;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoretext.text = "Score: " + score;
+    }
+}
