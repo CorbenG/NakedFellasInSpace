c704ab9a2bec52206530fb850dd1aa72ef990691
ash_kmn <kamensky@sheridancollege.ca>
2021-02-08 20:08:27 -0500

    add score increase animation

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -7,6 +7,14 @@ public class GameController : MonoBehaviour
 {
     public int score;
     public Text scoretext;
+    public GameObject textContainer;
+    public float scorePunchPower;
+    public float scoreBouncyness;
+    public float scoreSpeed;
+    public float scoreAmplitude;
+    float scoreAnimationPosition = 0.0f;
+    float scoreAnimationSpeed = 0.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +23,17 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        scoretext.text = "Score: " + score;
+        scoretext.text = score.ToString();
+        scoreAnimationSpeed = Mathf.Lerp(scoreAnimationSpeed, -scoreAnimationPosition * scoreBouncyness, Time.deltaTime * scoreSpeed);
+        scoreAnimationPosition += scoreAnimationSpeed * Time.deltaTime;
+        textContainer.GetComponent<RectTransform>().localScale = new Vector3(Mathf.Pow(scoreAmplitude,scoreAnimationPosition), Mathf.Pow(scoreAmplitude,-scoreAnimationPosition),1);
+        
     }
 
     public void updateScore (int scoring)
     {
         score += scoring;
+        scoreAnimationSpeed = scorePunchPower;
         //Animate here
     }
 }
