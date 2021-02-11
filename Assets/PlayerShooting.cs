2330a79e363e4207b8e53a48c540949775860987
CorbenG <61744834+CorbenG@users.noreply.github.com>
2021-02-11 16:56:32 -0500

    Animate Gun
    Implemented idle and shoot animations for gun.

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -15,6 +15,7 @@ public class PlayerShooting : MonoBehaviour
     public float DisplayLerpSpeed;
     public float StartingOffest;
     AudioSource GunSound;
+    public Animator anim;
     
     // Start is called before the first frame update
     void Start()
@@ -60,13 +61,14 @@ public class PlayerShooting : MonoBehaviour
         Vector3 ProjectileDirection = new Vector3(WorldMousePosition.x - transform.position.x, WorldMousePosition.y - transform.position.y, 0).normalized;
         Gun.transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2((WorldMousePosition.y - Gun.transform.position.y) * Graphics.transform.localScale.x, (WorldMousePosition.x - Gun.transform.position.x) * Graphics.transform.localScale.x) * Mathf.Rad2Deg);
         Graphics.transform.localScale = new Vector3(Mathf.Sign(ProjectileDirection.x)*1.0f,Graphics.transform.localScale.y,Graphics.transform.localScale.z);
-
+        anim.SetBool("Shoot", false);
         if (Input.GetMouseButtonDown(0))
         {
             GameObject NewProjectile = Instantiate(Projectile, transform.position + ProjectileDirection * StartingOffest, Quaternion.identity);
             NewProjectile.GetComponent<ProjectileMover>().MovementDirection = ProjectileDirection;
             NewProjectile.GetComponent<ProjectileMover>().AmmoType = AmmoType;
             // Debug.Log(AmmoType);
+            anim.SetBool("Shoot", true);
             GunSound.pitch = Random.Range(0.9f, 1.1f);
             GunSound.Play();
 
