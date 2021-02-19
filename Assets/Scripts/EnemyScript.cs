576f4445defe4750ff68298a76b7c9b3f1eb8cdc
ash_kmn <kamensky@sheridancollege.ca>
2021-02-18 22:07:59 -0500

    add requests and request completion

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -9,6 +9,9 @@ public class EnemyScript : MonoBehaviour
     public float maxSpeed;
     public float bouncePower;
     public GameObject spawnAnim;
+    public GameObject display_1;
+    public GameObject display_2;
+    public Color[] request_colors;
 
     GameController Game;
     Vector2 randomDir;
@@ -18,10 +21,22 @@ public class EnemyScript : MonoBehaviour
     SpriteRenderer sprite;
     float spawnTime = 0.1f;
     float spawnTimer = 0;
+
+    public int request_id1 = 0;
+    public int request_id2 = 0;
+    public bool needs_one = false;
+
+    bool id1_satisfied = false;
+    bool id2_satisfied = false;
     
     // Start is called before the first frame update
     void Start()
     {
+        id2_satisfied = needs_one;
+        request_id1 = Random.Range(0, 3);
+        request_id2 = Random.Range(0, 3);
+
+
         Game = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
         rb = GetComponent<Rigidbody2D>();
         transform.eulerAngles = new Vector3(0, 0, Random.Range(0f, 360f));
@@ -49,15 +64,19 @@ public class EnemyScript : MonoBehaviour
         {
             spawnTimer += Time.deltaTime;
         }
+        display_1.GetComponent<SpriteRenderer>().color = request_colors[request_id1];
+        display_2.GetComponent<SpriteRenderer>().color = request_colors[request_id2];
+        display_1.SetActive(!id1_satisfied);
+        display_2.SetActive(!id2_satisfied);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<ProjectileMover>() != null)
         {
-            Game.updateScore(100);
+           
             Destroy(collision.gameObject);
-            Destroy(this.gameObject);
+            get_clothes(collision.gameObject.GetComponent<ProjectileMover>().AmmoType);
         }
         if (collision.gameObject.tag == "WorldBorderTop")
         {
@@ -77,4 +96,22 @@ public class EnemyScript : MonoBehaviour
         newBounceSound.GetComponent<AudioSource>().volume = 0.5f;
     }
 
+    public void get_clothes(int id)
+    {
+        if ((id == request_id1) && !id1_satisfied)
+        {
+            id1_satisfied = true;
+            Game.updateScore(100);
+        } 
+        else if ((id == request_id2) && !id2_satisfied)
+        {
+            id2_satisfied = true;
+            Game.updateScore(100);
+        }
+
+        if (id1_satisfied && id2_satisfied)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
