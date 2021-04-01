4dc96fc5b15c2591d82d5f5ccf336720f352feb6
CorbenG <61744834+CorbenG@users.noreply.github.com>
2021-03-31 22:45:29 -0400

    Implement main menu

---
+++ Assets/HowToPlayController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HowToPlayController : MonoBehaviour
+{
+    public GameObject screen1;
+    public GameObject screen2;
+    public GameObject screen3;
+
+    int screen = 1;
+
+    public Button leftButtonObj;
+    public Button rightButtonObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(screen == 1)
+        {
+            leftButtonObj.interactable = false;
+            screen1.SetActive(true);
+            screen2.SetActive(false);
+            screen3.SetActive(false);
+        }
+        else if (screen == 2)
+        {
+            leftButtonObj.interactable = true;
+            rightButtonObj.interactable = true;
+            screen1.SetActive(false);
+            screen2.SetActive(true);
+            screen3.SetActive(false);
+        }
+        else if (screen == 3)
+        {
+            rightButtonObj.interactable = false;
+            screen1.SetActive(false);
+            screen2.SetActive(false);
+            screen3.SetActive(true);
+        }
+    }
+
+    public void leftButton()
+    {
+        screen--;
+    }
+
+    public void rightButton()
+    {
+        screen++;
+    }
+
+    public void exitScene()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LaunchGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
